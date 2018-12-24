48e5011bd925d55730cf5cf898b3f66c6ee646b4
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2018-12-24 21:17:26 +0530

    record: Spawn up shell which would record the commands
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

---
+++ record.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"github.com/kr/pty"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func test() error {
+
+	// Get the SHELL which is getting currently used
+	shell := os.Getenv("SHELL")
+	if shell == "" {
+		shell = "sh"
+	}
+
+	// Create arbitrary command.
+	c := exec.Command(shell)
+
+	// Start the command with a pty.
+	ptmx, err := pty.Start(c)
+	if err != nil {
+		return err
+	}
+	// Make sure to close the pty at the end.
+	defer func() { _ = ptmx.Close() }() // Best effort.
+
+	// Handle pty size.
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGWINCH)
+	go func() {
+		for range ch {
+			if err := pty.InheritSize(os.Stdin, ptmx); err != nil {
+				log.Printf("error resizing pty: %s", err)
+			}
+		}
+	}()
+	ch <- syscall.SIGWINCH // Initial resize.
+
+	// Set stdin in raw mode.
+	oldState, err := terminal.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = terminal.Restore(int(os.Stdin.Fd()), oldState) }() // Best effort.
+
+	// Copy stdin to the pty and the pty to stdout.
+	go func() { _, _ = io.Copy(ptmx, os.Stdin) }()
+	_, _ = io.Copy(os.Stdout, ptmx)
+
+	return nil
+}
+
+func main() {
+	if err := test(); err != nil {
+		log.Fatal(err)
+	}
+}
